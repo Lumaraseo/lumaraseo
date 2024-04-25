16d66cd0f535462345cf8843a36df8218d059621
JotaJotaM1 <johnjames_89@hotmail.com>
2024-04-25 13:07:20 -0500

    The name of the cookie that is saved is changed to LumaraSeo.

--- cookies.js
+++ cookies.js
@@ -2,22 +2,30 @@
 
 const cookieBox = document.querySelector(".wrapper");
 
-if (cookieBox) { // Verifica si cookieBox existe
-    const acceptBtn = cookieBox.querySelector("button");
+if (cookieBox) {
+  // Verifica si cookieBox existe
+  const acceptBtn = cookieBox.querySelector("button");
 
-    if (acceptBtn) { // Verifica si acceptBtn existe
-        acceptBtn.onclick = () => {
-            // Estableciendo la cookie por 1 mes, después de un mes expirará automáticamente
-            document.cookie = "CookieBy=CodingNepal; max-age=" + 60 * 60 * 24 * 30;
-            if (document.cookie) { // Si la cookie está establecida
-                cookieBox.classList.add("hide"); // Ocultar el cuadro de cookies
-            } else { // Si la cookie no está establecida, entonces alertar un error
-                alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
-            }
-        };
-    }
+  if (acceptBtn) {
+    // Verifica si acceptBtn existe
+    acceptBtn.onclick = () => {
+      // Estableciendo la cookie por 1 mes, después de un mes expirará automáticamente
+      document.cookie = "CookieBy=LumaraSEO; max-age=" + 60 * 60 * 24 * 30;
+      if (document.cookie) {
+        // Si la cookie está establecida
+        cookieBox.classList.add("hide"); // Ocultar el cuadro de cookies
+      } else {
+        // Si la cookie no está establecida, entonces alertar un error
+        alert(
+          "Cookie can't be set! Please unblock this site from the cookie setting of your browser."
+        );
+      }
+    };
+  }
 
-    let checkCookie = document.cookie.indexOf("CookieBy=CodingNepal"); // Revisando nuestra cookie
-    // Si la cookie está establecida entonces oculta el cuadro de cookies, si no, muéstralo
-    checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
+  let checkCookie = document.cookie.indexOf("CookieBy=LumaraSEO"); // Revisando nuestra cookie
+  // Si la cookie está establecida entonces oculta el cuadro de cookies, si no, muéstralo
+  checkCookie != -1
+    ? cookieBox.classList.add("hide")
+    : cookieBox.classList.remove("hide");
 }
